ffdc5a330194fddebfff74c82e7f1092f3164e99
Mehul Agarwal <agarwalmehul423@gmail.com>
2025-10-23 01:25:14 +0530

    cors bug fix

--- index.js
+++ index.js
@@ -8,7 +8,28 @@ const app = express();
 
 app.use(express.json());
 app.use(clerkMiddleware());
-app.use(cors({}));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/api/notes", notesRouter);
 
